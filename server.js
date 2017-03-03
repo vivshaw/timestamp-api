fcb3bb8be8a61a1ef0cfc351701f2ea66976843d
Hannah Vivian Shaw <hvivianshaw@gmail.com>
2017-03-03 00:22:42 +0000

    Write landing page & route

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 var express = require('express')
+var pug = require('pug')
+
 var app = express()
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'pug')
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.render('index')
 })
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
+  console.log('Timestamp app listening on port 8080!')
 })
